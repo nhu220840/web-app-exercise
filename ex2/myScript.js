10c33ed571c732c808aff5f24a6019c7a4914d38
nhu220840 <nhu220840@gmail.com>
2025-09-16 19:00:58 +0700

    first commit

---
+++ ex2/myScript.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML =
+        "Welcome to the World of JavaScript";
+}
+
+var num_value = 35.00;
+var string_value = "This is a number:" + num_value;
+var strValue = "4" + 3 + 1;
+var strValueTwo = 4 + 3 + "1";
+var firstResult = "35" - 3;
+// var str=String(value);
+
+console.log(strValue)
+console.log(strValueTwo)
+console.log(firstResult)
